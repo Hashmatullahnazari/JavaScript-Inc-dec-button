ddf062ef0774e4a043a7f2d1ece921de7ee51e94
Hashmatullah Nazari <hn0153@gmail.com>
2021-12-05 20:45:42 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+  var count = 0;
+
+
+  function decBtn() {
+
+      if (count => 0) {
+          count--;
+          document.getElementById("demo").innerHTML = count;
+
+      }
+  }
+
+  function incBtn() {
+      if (count => 0) {
+
+          count++;
+          document.getElementById("demo").innerHTML = count;
+
+      }
+  }
